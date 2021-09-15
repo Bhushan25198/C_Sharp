82f93d43b578b5d159eac1461ab1da6e5b20f490
Bhushan25198 <90633313+Bhushan25198@users.noreply.github.com>
2021-09-15 18:22:28 +0530

    Add files via upload

---
+++ Task_17/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserInfo u = new UserInfo("admin", "admin", "admin@gmail.com");
+            
+            int i = 0;
+
+            do
+            {
+                Console.WriteLine("Enter Username:");
+                u.UserName = Console.ReadLine();
+
+                Console.WriteLine("Enter Password:");
+                u.Password = Console.ReadLine();
+
+                Console.WriteLine("Enter EmailID:");
+                u.EmailID = Console.ReadLine();
+
+                if (u.UserName != "admin" || u.Password != "admin")
+                    i++;
+                else
+                    i = 1;
+            } while (u.UserName != "admin" || u.Password != "admin" && i != 3);
+            if (i == 3)
+            {
+                Console.WriteLine("You have three time attemted login. Please try again later....!!!!!!");
+            }
+            else
+            {
+                Console.WriteLine("You have logged in successfully.....Welcome to the portal!!!!!!!!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task_17/userinfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_17
+{
+    public class UserInfo
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string EmailID { get; set; }
+
+        public UserInfo(string username, string passwd, string mailid)
+        {
+            UserName = username;
+            Password = passwd;
+            EmailID = mailid;
+        }
+
+
+    }
+}
