15a94714aa2cc07b371305a349ad642faaf4315f
Bhushan25198 <90633313+Bhushan25198@users.noreply.github.com>
2021-09-15 18:12:06 +0530

    Add files via upload

---
+++ Task3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        private static string id;
+        private static string name;
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> d = new Dictionary<int, string>();
+            d.Add(0, "1");
+            d.Add(1, "Bhushan");
+            d.Add(2, "25");
+
+            foreach (KeyValuePair<int, string> item in d)
+            {
+                Console.WriteLine("key {0} and value {1}",item.Key, item.Value);
+
+            }
+
+            Console.WriteLine("Enter key to delete");
+            int k = Convert.ToInt32(Console.ReadLine());
+            if (!d.ContainsKey(k))
+            {
+                Console.WriteLine("key does not exist");
+            }
+            else
+            {
+                d.Remove(k);
+                Console.WriteLine("key removed...");
+            }
+
+            foreach (KeyValuePair<int, string> D in d)
+
+            {
+                Console.WriteLine("key: {0}, Value: {1}",D.Key, D.Value);
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
